9a1b278b0421c0755866037a0477271d59ec922a
BABARSHAH786 <159919873+BABARSHAH786@users.noreply.github.com>
2024-12-13 20:12:48 +0500

    update code new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 // import Image from "next/image";
 import Footer from './components/footer';
 import Navbar from './components/Navbar'
-import TopfourHappy from './components/TopfourHappy';
-import Topone from './components/Topone';
-import TopThreeBrowse from './components/TopthreeBrowse';
-import TopTwoVersace from './components/Toptwoversace';
+
 
 export default function Home() {
   return (
    <div>
     <Navbar />
-    <Topone />
-    <TopTwoVersace />
-    <TopThreeBrowse />
-    <TopfourHappy />
+
     <Footer />
 
 
